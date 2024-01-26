ff30b4a39569931ca3ad7bc3a495f16917214502
martelwilliams <martel.williams11@gmail.com>
2024-01-25 22:46:48 -0600

    adding madlibs project

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+word3 = input("word: ")
+word4 = input("word: ")
+
+
+madlib = (f"Python is {adj1}. You have to {adj2} what it can do. If you {word3} like me you will {word4} 1 2 3")
+
+print(madlib)
